43b0b00321b038e758eb9b871de2b48b7c62f68e
vavido <matz.weirich@gmail.com>
2017-03-06 19:06:35 +0100

    new layout

--- app/src/main/java/com/eehack/visualink/MainActivity.java
+++ app/src/main/java/com/eehack/visualink/MainActivity.java
@@ -3,6 +3,7 @@ package com.eehack.visualink;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewFlipper flipper = (ViewFlipper) findViewById(R.id.)
 
     }
 }
